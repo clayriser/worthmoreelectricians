69fa9a4c448c1704916914b93b81149b5c928c4a
Clay Riser <goldenco.owner@mrelectric.com>
2026-07-21 18:37:31 -0600

    Created js folder and analytics.js
    Created js folder and analytics.js file and removed js from index.html

---
+++ js/analytics.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('a[data-track][href^="tel:"]').forEach((link) => {
+    link.addEventListener('click', () => {
+        gtag('event', 'phone_click', {
+            button_location: link.dataset.track,
+            button_text: link.textContent.trim()
+        });
+    });
+});
